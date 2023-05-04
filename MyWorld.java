a8710e64f2c133645f2060fdfcc82212e9e6931a
Adrian400811 <adrianleeho@gmail.com>
2023-05-04 09:42:11 -0400

    Added function for Elephant.java

--- MyWorld.java
+++ MyWorld.java
@@ -16,6 +16,8 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(1280, 720, 1);
+        Elephant e = new Elephant();
+        addObject(e, 100, 700);
     }
 }

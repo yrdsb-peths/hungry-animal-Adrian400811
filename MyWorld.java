d809e943cf7dbb2ef18a1865b0036b597771960e
Adrian400811 <adrianleeho@gmail.com>
2023-05-25 09:31:11 -0400

    added animations

--- MyWorld.java
+++ MyWorld.java
@@ -18,19 +18,25 @@ public class MyWorld extends World
     {    
         // Create a new world with 1280x720 cells with a cell size of 1x1 pixels.
         super(1280, 720, 1);
+        GreenfootImage bg = new GreenfootImage("Geometry.png");
+        bg.scale(getWidth(), getHeight());
+        setBackground(bg);
         Elephant e = new Elephant();
-        sL = new Label(0, 80);
-        addObject(e, 100, 700);
-        addObject(sL, 50, 50);
+        sL = new Label("Score: "+score, 80);
+        addObject(e, 100, 650);
+        addObject(sL, 150, 50);
         createApple();
     }
     public void inScore(){
         score++;
-        sL.setValue(score);
+        sL.setValue("Score: "+score);
     }
     public void kill(){
         Label goL = new Label("YOU DUMB", 100);
         addObject(goL, 640, 360);
+        Label goS = new Label("Your Score: "+score, 50);
+        addObject(goS, 640, 460);
+        removeObject(sL);
     }
     public void createApple(){
         Apple a = new Apple();
